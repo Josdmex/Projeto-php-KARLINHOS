d24bb6bc48a01cec5799ef5c919da331b960b0ec
Josdmex <costakaique440@gmail.com>
2026-09-02 06:26:58 -0400

    Delete biblioteca_segura directory

--- biblioteca_segura/includes/footer.php
+++
@@ -1,9 +0,0 @@
-</main>
-<footer class="rodape">
-    <div class="container">
-        <span><i class="bi bi-geo-alt"></i> Rua das Palmeiras, 240 &nbsp;|&nbsp; <i class="bi bi-telephone"></i> (66) 3333-1200</span>
-        <span>&copy; <?php echo date('Y'); ?> Biblioteca Municipal</span>
-    </div>
-</footer>
-</body>
-</html>
